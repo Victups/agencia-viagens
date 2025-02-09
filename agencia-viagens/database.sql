671e094fcd86e65fb5769955ec510f5fdea837e9
Victor Pereira <157552390+Victups@users.noreply.github.com>
2025-02-09 13:34:10 -0300

    Update database.sql

--- agencia-viagens/database.sql
+++ agencia-viagens/database.sql
@@ -1,23 +1,50 @@
-CREATE TABLE Cliente (
-  id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  telefone VARCHAR(15)
-);
+CREATE TABLE Cliente 
+( 
+ ClienteID INT NOT NULL,  
+ Nome VARCHAR(n) NOT NULL,  
+ Email VARCHAR(n) NOT NULL,  
+ Telefone VARCHAR(n) NOT NULL,  
+ DataNascimento DATE,  
+ UNIQUE (Email)
+); 
 
-CREATE TABLE Destino (
-  id_destino INT AUTO_INCREMENT PRIMARY KEY,
-  nome VARCHAR(50) NOT NULL,
-  descricao TEXT,
-  preco DECIMAL(10, 2) NOT NULL
-);
+CREATE TABLE Viagem 
+( 
+ ViagemID INT AUTO_INCREMENT,  
+ Destino VARCHAR(n) NOT NULL,  
+ Descricao CHAR(n) NOT NULL,  
+ DataPartida DATE AUTO_INCREMENT,  
+ DataRetorno DATE AUTO_INCREMENT,  
+); 
 
-CREATE TABLE Reserva (
-  id_reserva INT AUTO_INCREMENT PRIMARY KEY,
-  data_viagem DATE NOT NULL,
-  qtd_pessoas INT NOT NULL,
-  id_cliente INT NOT NULL,
-  id_destino INT NOT NULL,
-  FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
-  FOREIGN KEY (id_destino) REFERENCES Destino(id_destino)
-);
+CREATE TABLE Reserva 
+( 
+ ReservaID INT AUTO_INCREMENT,  
+ ClienteID INT,  
+ ViagemID INT,  
+ DataReserva DATE NOT NULL,  
+); 
+
+CREATE TABLE Voo 
+( 
+ VooID INT AUTO_INCREMENT,  
+ ViagemID INT,  
+ Origem VARCHAR(n) NOT NULL,  
+ Destino VARCHAR(n) NOT NULL,  
+ DataHoraPartida DATE,  
+ DataHoraChegada DATE,  
+); 
+
+CREATE TABLE Pagamento 
+( 
+ PagamentoID INT AUTO_INCREMENT,  
+ ReservaID INT,  
+ Valor FLOAT NOT NULL,  
+ DataPagamento DATE AUTO_INCREMENT,  
+); 
+
+ALTER TABLE Cliente ADD FOREIGN KEY(ClienteID) REFERENCES undefined (ClienteID)
+ALTER TABLE Reserva ADD FOREIGN KEY(ClienteID) REFERENCES Cliente (ClienteID)
+ALTER TABLE Reserva ADD FOREIGN KEY(ViagemID) REFERENCES Viagem (ViagemID)
+ALTER TABLE Voo ADD FOREIGN KEY(ViagemID) REFERENCES Viagem (ViagemID)
+ALTER TABLE Pagamento ADD FOREIGN KEY(ReservaID) REFERENCES Reserva (ReservaID)
